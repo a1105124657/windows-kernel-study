eb8e3ba1b677ac0404f51f4f8afe4843c0ae692d
sbb <52585693+b917893200@users.noreply.github.com>
2020-02-04 17:49:45 +0800

    Update win7x64驱动修改进程名driver.c

--- win7x64驱动修改进程名driver.c
+++ win7x64驱动修改进程名driver.c
@@ -8,7 +8,14 @@ void DriverUnLoad(PDRIVER_OBJECT pDriverObject)
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
 	DriverObject->DriverUnload = DriverUnLoad;
+	//cheatengine-x86_64.exe   截取15个字节 cheatengine-x8
 	PEPROCESS pProcess = EnumProcess("cheatengine-x8");
-	ChangeProcessName(pProcess);
+	if ((pProcess) == (unsigned long long*)-1)
+	{
+		DbgPrint("error:没找到进程......");
+		return STATUS_UNSUCCESSFUL;
+	}
+	ChangePath(pProcess);
+	//ChangeProcessName(pProcess);
 	return STATUS_SUCCESS;
 }
